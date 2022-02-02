6bca72e4e82206c461191c39f3f2d15d4d2d5994
Amr Saher <79362856+AmrSaher@users.noreply.github.com>
2022-02-02 16:52:52 +0200

    Update script.js

--- AmrSaher/script.js
+++ AmrSaher/script.js
@@ -4,13 +4,12 @@ const hl = document.querySelectorAll('.header .links a');
 const body = document.body;
 const toggleThemeBtn = document.querySelector('#toggle-theme');
 let theme = window.localStorage.getItem('theme');
-
-if (!theme) {
-    window.localStorage.setItem("theme", "light");
-    theme = window.localStorage.getItem('theme');
+if (theme == null) {
+    theme = window.localStorage.setItem('theme', 'light');
+    setTheme(theme);
+} else {
+    setTheme(theme);
 }
-
-setTheme(theme);
 
 for (let i = 0; i < hl.length; i++) {
     let link = hl[i];
