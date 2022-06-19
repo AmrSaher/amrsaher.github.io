311f0ca8dbe68b9ac54788859432d4452b443ece
AmrSaher <amrosaher1234@gmail.com>
2022-06-19 14:43:07 -0700

    Make it responsive

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 let header = document.querySelector('header');
 let socialMediaLinks = document.querySelectorAll('.social-media box-icon');
+let toggle = document.querySelector('.toggle');
+let links = document.querySelector('header .links')
+
+toggle.addEventListener('click', () => {
+    links.classList.toggle('active');
+});
 
 window.onscroll = function () { 
     if (scrollY >= 100) {
