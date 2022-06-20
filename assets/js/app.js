fbf2530912c77035b2415aaf76af920cac390fcf
AmrSaher <amrosaher1234@gmail.com>
2022-06-20 11:04:45 -0700

    Create scroll indicator

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,18 @@
 let header = document.querySelector('header');
 let socialMediaLinks = document.querySelectorAll('.social-media box-icon');
 let toggle = document.querySelector('.toggle');
-let links = document.querySelector('header .links')
+let links = document.querySelector('header .links');
+let bar = document.getElementById("bar");
 
 toggle.addEventListener('click', () => {
     links.classList.toggle('active');
 });
 
-window.onscroll = function () { 
+window.onscroll = function () {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    bar.style.width = scrolled + "%";
     if (scrollY >= 100) {
         header.classList.add('active');
         socialMediaLinks.forEach(link => {
